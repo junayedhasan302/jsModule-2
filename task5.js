76dd123e4f0cfc7685ea60165066b8bf1e83dee2
junayedhasan302 <junayedhasan302@gmail.com>
2026-07-19 12:02:02 +0600

    Task5

---
+++ task5.js
@@ -0,0 +1,5 @@
+// Take a number variable and check whether it is positive or negative (using if-else).
+
+let num = 10;
+if(num>0) console.log("Positive Number");
+else console.log("Negativee Number");
