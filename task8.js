81695dff04d9bb66a7afc64f664ff80e543d143e
junayedhasan302 <junayedhasan302@gmail.com>
2026-07-19 12:22:29 +0600

    Task9

---
+++ task8.js
@@ -0,0 +1,5 @@
+// Using age and hasTicket, print "Entry allowed" if age is above 18 AND the person has a ticket (use &&).
+let age = 18, hasTicket = true;
+if(age>=18 && hasTicket == true){
+    console.log("Entry Allowed!");
+}
---
+++ task9.js
@@ -0,0 +1,6 @@
+// Using isWeekend and isHoliday, print "No work today" if either one is true (use ||).
+let isWeekend = true, isHoliday = false;
+if(isWeekend == true || isHoliday == true){
+    console.log("No work today!");
+}
+else console.log("Go go office");
