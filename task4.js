a968a9e668719a105e4467a025a96f1656f8d25d
junayedhasan302 <junayedhasan302@gmail.com>
2026-07-19 11:59:16 +0600

    Task4

---
+++ task4.js
@@ -0,0 +1,8 @@
+// Take a variable stock. If stock is 0, print "Out of stock" (use only if, no else).
+
+// let stock = Number(prompt("Enter stock:"));
+// Check if stock is 0
+let stock = 0;
+if (stock === 0) {
+    console.log("Out of stock");
+}
